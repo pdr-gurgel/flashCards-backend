4ab57d10db15afe5d9b33abeec0cca479e84097c
pdr-gurgel <pedrohgr0208@gmail.com>
2025-07-11 18:07:59 -0300

    feat/refatoredControllers

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,21 +6,29 @@ class AuthController {
         this.login = this.login.bind(this);
     }
 
+    /**
+     * Autentica um usuário e retorna token JWT
+     * @param {FastifyRequest} req - Requisição Fastify
+     * @param {FastifyReply} reply - Resposta Fastify
+     */
     async login(req, reply) {
         const { email, password } = req.body;
+
         if (!email || !password) {
             return reply.code(400).send({ error: 'Email e senha são obrigatórios.' });
         }
+
         try {
             const { token, user } = await this.authService.loginUser(email, password);
-            return reply.send({ token, user });
+            return reply.code(200).send({ token, user });
         } catch (err) {
             if (err.message === 'Credenciais inválidas') {
                 return reply.code(401).send({ error: 'Credenciais inválidas.' });
             }
+            console.error('Erro ao realizar login:', err);
             return reply.code(500).send({ error: 'Erro interno do servidor.' });
         }
     }
 }
 
-export default AuthController; 
+export default AuthController;

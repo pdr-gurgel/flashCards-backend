de03372a55955b4a5b46d3a7003f9deed72a2561
pdr-gurgel <pedrohgr0208@gmail.com>
2025-07-11 22:31:01 -0300

    feat/implementPoolDatabase

--- src/models/db.js
+++ src/models/db.js
@@ -4,7 +4,10 @@ import { Pool } from 'pg';
 const client = new Pool({
     connectionString: config.database.url,
     max: 20,
-    idleTimeoutMillis: 20000
+    idleTimeoutMillis: 20000,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 await client.connect();
